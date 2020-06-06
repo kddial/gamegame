e25658e0b672397abb974390638b93ab9eabe8a5
Kevin Dial <kevin.dial.phone@gmail.com>
2020-06-06 02:18:44 -0400

    Post new visit on new session

--- client/src/visits-ui.ts
+++ client/src/visits-ui.ts
@@ -1,4 +1,6 @@
 const NUM_VISITS_URL = '/visits';
+const NEW_VISIT_URL = '/new-visit';
+const SESSION_VISIT_ACKNOWLEDGED = 'SESSION_VISIT_ACKNOWLEDGED';
 
 fetch(NUM_VISITS_URL)
   .then((response) => {
@@ -10,3 +12,16 @@ fetch(NUM_VISITS_URL)
     const visitsDiv = document.getElementById('visits');
     visitsDiv.innerHTML = `Total visits: ${visits}`;
   });
+
+if (!!sessionStorage.getItem(SESSION_VISIT_ACKNOWLEDGED) === false) {
+  fetch(NEW_VISIT_URL, {
+    method: 'POST',
+  })
+    .then((response) => {
+      return response.text();
+    })
+    .then((response) => {
+      console.log('new visit', response);
+      sessionStorage.setItem(SESSION_VISIT_ACKNOWLEDGED, '1');
+    });
+}
