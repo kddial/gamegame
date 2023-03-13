c84e0cd664f6e6f114dd5a99f004ff8f030cf5b6
Kevin Dial <kevin.dial.phone@gmail.com>
2023-03-13 16:40:11 -0400

    add domain to create local cert

--- create-local-certs.sh
+++ create-local-certs.sh
@@ -3,4 +3,4 @@
 # used to make localhost certs for development
 
 mkcert -install
-mkcert -cert-file ~/.gamegame/https.cert -key-file ~/.gamegame/https.key localhost 127.0.0.1 ::1
+mkcert -cert-file ~/.gamegame/https.cert -key-file ~/.gamegame/https.key "gamegame.kevindial.com" localhost 127.0.0.1 ::1
