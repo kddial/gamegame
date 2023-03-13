60e425a41d33bbdd4656292858faf53225460381
Kevin Dial <kevin.dial.phone@gmail.com>
2023-03-13 16:08:53 -0400

    upload scripts for railway.app

---
+++ railway-build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# scripts for https://railway.app/
+
+cd client
+npm install
+cd ..
+cd server
+npm install
+npm run build
---
+++ railway-start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# scripts for https://railway.app/
+
+# sudo ./copy-https-certs.sh
+
+cd server
+cd dist
+node index.js
