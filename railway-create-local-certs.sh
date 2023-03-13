dce1d5be90703259b93b46827f6b610ba0798116
Kevin Dial <kevin.dial.phone@gmail.com>
2023-03-13 17:23:14 -0400

    upload linux exe,  maybe this will work?

--- railway-create-local-certs.sh
+++ railway-create-local-certs.sh
@@ -2,5 +2,5 @@
 
 # used to make localhost certs for development
 
-mkcert -install
-mkcert -cert-file ./certs/https.cert -key-file ./certs/https.key "gamegame.kevindial.com" localhost 127.0.0.1 ::1
+./bin/mkcert-v1_4_4-linux-amd64 -install
+./bin/mkcert-v1_4_4-linux-amd64 -cert-file ./certs/https.cert -key-file ./certs/https.key "gamegame.kevindial.com" localhost 127.0.0.1 ::1
