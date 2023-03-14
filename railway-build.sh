7c901742967ca16dfb642ac3b58ea74e6dc717ff
Kevin Dial <kevin.dial.phone@gmail.com>
2023-03-13 23:55:38 -0400

    delete anything relating to SSL https server

--- railway-build.sh
+++ railway-build.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 # scripts for https://railway.app/
 
-sudo ./railway-create-local-certs.sh
-
 cd client
 npm install
 cd ..
--- railway-create-local-certs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# used to make localhost certs for development
-
-mkdir -p ./certs
-./bin/mkcert-v1_4_4-linux-amd64 -install
-./bin/mkcert-v1_4_4-linux-amd64 -cert-file ./certs/https.cert -key-file ./certs/https.key "gamegame.kevindial.com" "gamegame-production.up.railway.app" localhost 127.0.0.1 0.0.0.0 ::1
